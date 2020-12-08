719ac4ed0a3c54f79b59379a685c33e1d8148178
Aleksandre Beridze <sandro.beridze65@gmail.com>
2020-12-08 18:51:12 +0400

    project start

---
+++ Assets/Scripts/enemyAi.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyAi : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+    private void EngageTarget()
+    {
+        if (distanceToTarget > navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+        GetComponent<Animator>().SetBool("attack", false);
+        GetComponent<Animator>().SetTrigger("move");
+
+
+    }
+    private void AttackTarget()
+    {
+        GetComponent<Animator>().SetBool("attack", true);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+
+    }
+}
