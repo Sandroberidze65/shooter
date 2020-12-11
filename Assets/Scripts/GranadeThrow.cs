477921877a05f4c49f0d6466d5f066de43220966
Aleksandre Beridze <sandro.beridze65@gmail.com>
2020-12-11 22:48:05 +0400

    Add rocket launcher and granade to player

---
+++ Assets/Scripts/GranadeThrow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GranadeThrow : MonoBehaviour
+{
+    public float throwForce = 10f;
+    public GameObject grenadePrefab;
+
+    int MaxGrenage = 5;
+    int CurrentGrenade = -1;
+    bool enter = true;
+
+
+    void Start()
+    {
+        if (CurrentGrenade == -1)
+        {
+            CurrentGrenade = MaxGrenage;
+        }
+
+    }
+
+    void OnGUI()
+    {
+
+        string count = " Grenade: " + CurrentGrenade;
+        if (enter)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 300, Screen.height - 100, 250, 30), count);
+        }
+
+
+    }
+
+    void Update()
+    {
+
+        if (CurrentGrenade <= 0)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Throwexplode();
+        }
+    }
+
+    void Throwexplode()
+    {
+        CurrentGrenade--;
+        GameObject Grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+        Rigidbody rb = Grenade.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+    }
+}
---
+++ Assets/Scripts/granade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class granade : MonoBehaviour
+{
+    public float delay=3f;
+    bool hasExploded=false;
+
+    public GameObject explosionEffect;
+
+    float countdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown=delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0 && !hasExploded){
+            Debug.Log("Hello");
+            hasExploded=true;
+            Explode();
+            
+        }
+
+    }
+    int i=0;
+
+    void Explode(){
+        
+        Instantiate(explosionEffect,transform.position, transform.rotation);
+        Debug.Log(i++);
+
+        Destroy(gameObject);
+        
+    }
+}
